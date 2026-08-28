23147c68ff7f2b02735adc3df3ae518246833cc1
UT3456 <utkarshsingh22233@gmail.com>
2026-08-28 20:31:40 +0530

    Time: 2193 ms (37.67%) | Memory: 42.8 MB (75.97%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -2,32 +2,37 @@ import java.util.Arrays;
 
 class Solution {
 
-    public boolean isPalindrome(String str, int i, int j) {
-        while (i <= j) {
-            if (str.charAt(i) != str.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+    private int[] dp;
+
+    public int minCut(String s) {
+        int n = s.length();
+        dp = new int[n];
+
+        Arrays.fill(dp, -1);
+
+        return solve(s, 0, n - 1);
     }
 
-    public int solve(String str, int i, int j, int[] dp) {
+    private int solve(String s, int i, int j) {
 
-        if (i >= j) {
+        // No cut needed
+        if (i >= j || isPalindrome(s, i, j)) {
             return 0;
         }
 
+        // Already computed
         if (dp[i] != -1) {
             return dp[i];
         }
 
         int ans = Integer.MAX_VALUE;
 
-        for (int k = i; k <= j - 1; k++) {
-            if (isPalindrome(str, i, k)) {
-                int tempAns = 1 + solve(str, k + 1, j, dp);
+        for (int k = i; k < j; k++) {
+
+            // Only partition if left part is palindrome
+            if (isPalindrome(s, i, k)) {
+
+                int tempAns = 1 + solve(s, k + 1, j);
                 ans = Math.min(ans, tempAns);
             }
         }
@@ -35,12 +40,17 @@ class Solution {
         return dp[i] = ans;
     }
 
-    public int minCut(String s) {
-        int n = s.length();
+    private boolean isPalindrome(String s, int i, int j) {
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
 
-        return solve(s, 0, n, dp) - 1;
+            i++;
+            j--;
+        }
+
+        return true;
     }
 }
