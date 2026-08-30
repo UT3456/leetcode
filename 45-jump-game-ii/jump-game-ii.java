48ecb9e95b75f12ef314f67859bb911d0a964a16
UT3456 <utkarshsingh22233@gmail.com>
2026-08-30 17:06:15 +0530

    Time: 38 ms (11.60%) | Memory: 47.3 MB (38.69%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,8 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        int []dp=new int [nums.length];
-        Arrays.fill(dp,-1);
-        return solve(nums,0,dp);
+        // int []dp=new int [nums.length];
+        // Arrays.fill(dp,-1);
+        // return solve(nums,0,dp);
+        int dp[]=new int [nums.length];
+        dp[nums.length-1]=0;//no jump needed alreadt at zero 
+      
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            //jumps
+            int min=(int)(1e9);
+            for(int j=1;j<=nums[i];j++)
+            {   
+              if(i+j<nums.length)  min=Math.min(min,1+dp[i+j]);
+            }
+            dp[i]=min;
+        }
+        return dp[0];
     }
     private int solve(int arr[],int i,int []dp)
     {
