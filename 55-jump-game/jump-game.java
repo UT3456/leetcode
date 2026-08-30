5ef31ff2ea57fe030132ab06ce741ac2529c8cbf
UT3456 <utkarshsingh22233@gmail.com>
2026-08-30 16:09:52 +0530

    Time: 1660 ms (5.06%) | Memory: 48.2 MB (8.57%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean dp[]=new boolean [nums.length];
+        dp[nums.length-1]=true;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+           for(int j=1;j<=nums[i];j++)
+           {
+            if(i+j < nums.length && dp[i+j]) 
+            {
+                dp[i]=true;
+            }
+          
+           }
+        }
+        return dp [0];
+    }
+}
