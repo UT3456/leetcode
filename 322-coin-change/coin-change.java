069c2ce080e93c67995508af7bbf737ed7128056
UT3456 <utkarshsingh22233@gmail.com>
2026-09-17 20:25:12 +0530

    Time: 35 ms (13.10%) | Memory: 47.6 MB (8.14%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+         int dp[][]=new int [coins.length+1][amount+1];
+         for(int arr[]:dp){
+            Arrays.fill(arr,-1);
+         }
+                 int res=solve(coins,amount,0,dp);
+        return  res==(int)(1e9)?-1:res;
+    }
+
+    private int solve(int coins[], int amount, int i,int[][]dp) {
+        if (amount==0)
+            return 0;
+        if(i>=coins.length && amount>0)
+        {
+            //that means i cannot for,
+            return (int)(1e9);
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        
+            //this mean i dont have any coins 
+        
+        int take = (int)(1e9);
+        //i can only take if it is less than the amount
+
+        if (coins[i] <= amount)
+            take = 1 + solve(coins, amount - coins[i], i,dp);
+        int skip = solve(coins, amount, i + 1,dp);
+        return dp[i][amount]=Math.min(take, skip);
+
+    }
+}
